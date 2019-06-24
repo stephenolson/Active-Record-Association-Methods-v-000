d8c44617ccb31a9574ef771be9cf5c7c3bcaf183
Stephen Olson <stephenolson@gmail.com>
2019-06-24 22:13:35 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,7 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
-    self.artists.map {|artist| artist.name}
+    self.genre.map {|genre| genre.name}
   end
 
   def drake_made_this
